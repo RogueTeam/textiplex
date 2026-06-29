59b56b469975083cf0d1f26370c444371f85a95a
Antonio <antoniojosedonishung@gmail.com>
2026-06-29 14:55:49 -0400

    fix: optimized hot path of update score of bm25

--- query/searcher_bm25.go
+++ query/searcher_bm25.go
@@ -28,38 +28,54 @@ func (s *Searcher) UpdateScoresWithBM25(ctx *QueryContext, state *ClauseState) {
 	docLengths := field.DocumentLengths
 	freqs := s.Storage.TokenFrequencies[token.FrequenciesIndex : token.FrequenciesIndex+token.FrequencyCount]
 
+	var saturation, lengthPenalty float64
+	if s.BM25Saturation != 0 {
+		saturation = s.BM25Saturation
+	} else {
+		saturation = DefaultSaturation
+	}
+
+	if s.BM25LengthPenalty != 0 {
+		lengthPenalty = s.BM25LengthPenalty
+	} else {
+		lengthPenalty = DefaultLengthPenalty
+	}
+
 	var docIdxs [ManyIteratorBatchSize]uint32
 	it := ctx.Bitmap.ManyIterator()
-
+loop:
 	for {
 		n := it.NextMany(docIdxs[:])
 
 		for _, docIdx := range docIdxs[:n] {
-			docLengthIdx, found := slices.BinarySearchFunc(docLengths, docIdx, func(e storage.DocumentLengthEntry, t uint32) int { return cmp.Compare(e.Index, t) })
-			if !found {
-				continue
-			}
-			docLength := docLengths[docLengthIdx].Length
-			docLengths = docLengths[1+docLengthIdx:]
-
-			freqIdx, found := slices.BinarySearchFunc(freqs, docIdx, func(e storage.TokenFrequencyEntry, t uint32) int { return cmp.Compare(e.DocumentIndex, t) })
-			if !found {
-				continue
-			}
-			freq := freqs[freqIdx].Frequency
-			freqs = freqs[1+freqIdx:]
-
-			var saturation, lengthPenalty float64
-			if s.BM25Saturation != 0 {
-				saturation = s.BM25Saturation
+			var freq uint64
+			if len(freqs) == len(s.Storage.DocumentsIds) {
+				freq = freqs[docIdx].Frequency
 			} else {
-				saturation = DefaultSaturation
+				freqIdx, found := slices.BinarySearchFunc(freqs, docIdx, func(e storage.TokenFrequencyEntry, t uint32) int { return cmp.Compare(e.DocumentIndex, t) })
+				if !found && freqIdx < len(freqs) {
+					freqs = freqs[freqIdx:]
+					continue
+				} else if !found {
+					break loop
+				}
+				freq = freqs[freqIdx].Frequency
+				freqs = freqs[1+freqIdx:]
 			}
 
-			if s.BM25LengthPenalty != 0 {
-				lengthPenalty = s.BM25LengthPenalty
+			var docLength uint64
+			if len(docLengths) == len(s.Storage.DocumentsIds) {
+				docLength = docLengths[docIdx].Length
 			} else {
-				lengthPenalty = DefaultLengthPenalty
+				docLengthIdx, found := slices.BinarySearchFunc(docLengths, docIdx, func(e storage.DocumentLengthEntry, t uint32) int { return cmp.Compare(e.Index, t) })
+				if !found && docLengthIdx < len(docLengths) {
+					docLengths = docLengths[docLengthIdx:]
+					continue
+				} else if !found {
+					break loop
+				}
+				docLength = docLengths[docLengthIdx].Length
+				docLengths = docLengths[1+docLengthIdx:]
 			}
 
 			ctx.Scores[docIdx] += state.Boost * ScoreTermBM25(

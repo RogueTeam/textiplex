a786f7fc69d41d6419eb8e0fe0f530ed483bb8cb
Antonio <antoniojosedonishung@gmail.com>
2026-07-05 12:28:44 -0400

    fix: fields ordering done by consuming a pending bitmap

--- query/searcher_fields.go
+++ query/searcher_fields.go
@@ -1,6 +1,8 @@
 package query
 
-import "github.com/RoaringBitmap/roaring"
+import (
+	"github.com/RoaringBitmap/roaring"
+)
 
 func (s *Searcher) FieldScore(ctx *QueryContext, fieldHash uint64) {
 	field, found := s.Storage.Fields[fieldHash]
@@ -11,41 +13,25 @@ func (s *Searcher) FieldScore(ctx *QueryContext, fieldHash uint64) {
 	cardinality := ctx.Bitmap.GetCardinality()
 	ctx.Scores = make(map[uint32]float64, cardinality)
 
-	var docIdxs [ManyIteratorBatchSize]uint32
+	var pending = ctx.Bitmap.Clone()
 
-	var bitmapForPostingListRetrieval roaring.Bitmap
+	var retrievalBitmap roaring.Bitmap
 	for tokenIdx := range field.Tokens {
+		if pending.IsEmpty() {
+			break
+		}
+
 		token := &field.Tokens[tokenIdx]
 
-		s.Storage.PostingLists[token.PostingListIndex].UnsafeBitmap(&bitmapForPostingListRetrieval)
+		s.Storage.PostingLists[token.PostingListIndex].UnsafeBitmap(&retrievalBitmap)
 
-		var iterableBitmap, checkBitmap *roaring.Bitmap
-		if bitmapForPostingListRetrieval.GetCardinality() > ctx.Bitmap.GetCardinality() {
-			iterableBitmap = &ctx.Bitmap
-			checkBitmap = &bitmapForPostingListRetrieval
-		} else {
-			iterableBitmap = &bitmapForPostingListRetrieval
-			checkBitmap = &ctx.Bitmap
-		}
-
-		it := iterableBitmap.ManyIterator()
-		for {
-			n := it.NextMany(docIdxs[:])
-			for _, docIdx := range docIdxs[:n] {
-				if !checkBitmap.Contains(docIdx) {
-					continue
-				}
-
-				_, found := ctx.Scores[docIdx]
-				if found {
-					continue
-				}
+		for _, docIdx := range roaring.FastAnd(pending, &retrievalBitmap).ToArray() {
+			_, found := ctx.Scores[docIdx]
+			if !found {
 				ctx.Scores[docIdx] = float64(cardinality - uint64(len(ctx.Scores)))
 			}
-
-			if n < len(docIdxs) {
-				break
-			}
 		}
+
+		pending.Xor(&retrievalBitmap)
 	}
 }

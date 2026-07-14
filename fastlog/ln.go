8d095ff65977cb7af183a1d93758fa0928019866
Antonio <antoniojosedonishung@gmail.com>
2026-07-13 20:57:33 -0400

    fix: use of agner fog bucket grouping of math operations

--- fastlog/ln.go
+++ fastlog/ln.go
@@ -13,8 +13,10 @@ func Ln(x float32) (ro float32) {
 	// return -1.49278+(2.11263+(-0.729104+0.10969*x)*x)*x+0.6931471806*t;
 	bx := *(*uint32)(unsafe.Pointer(&x))
 	ex := bx >> 23
-	t := int(ex) - 127
+	t := float32(int(ex) - 127)
 	bx = 1065353216 | (bx & 8388607)
-	x = *(*float32)(unsafe.Pointer(&bx))
-	return -1.49278 + (2.11263+(-0.729104+0.10969*x)*x)*x + 0.6931471806*float32(t)
+	x1 := *(*float32)(unsafe.Pointer(&bx))
+	x2 := x1 * x1
+	x3 := x1 * x1 * x1
+	return (2.11263*x1 + -0.729104*x2) + (0.10969*x3 + 0.6931471806*t) + -1.49278
 }
